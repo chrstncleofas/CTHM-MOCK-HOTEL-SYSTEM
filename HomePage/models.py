b89a136178afd9ac01f2c304d25af3b6beb40ac1
Dev_tian <c.cleofas@psp.team>
2023-01-21 07:22:14 +0800

    New Functions

--- HomePage/models.py
+++ HomePage/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Reservation(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    address = models.CharField(max_length=70)
+    city = models.CharField(max_length=70)
+    province = models.CharField(max_length=70)
+    postal = models.PositiveBigIntegerField()
+    birthday = models.CharField(max_length=50)
+    gender = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    phone = models.PositiveBigIntegerField()
+
+    def __str__(self):
+        return f'Reservation: {self.fname} {self.lname}'
+    
