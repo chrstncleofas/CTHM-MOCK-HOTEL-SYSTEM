c69dbcab44c959517726175f5843f359ee3b500b
Dev_tian <c.cleofas@psp.team>
2023-01-21 09:45:30 +0800

    fix the issue and errors in the system

--- HomePage/forms.py
+++ HomePage/forms.py
@@ -1,5 +1,19 @@
 from django import forms
 from .models import Reservation
 
-class Reservation_Form:
-    pass
+class Reservation_Form(forms.ModelForm):
+    class Meta:
+        model = Reservation
+        fields = ['fname', 'lname', 'address', 'city', 'province', 'postal', 'birthday', 'gender', 'email', 'phone']
+        labels = {
+            'fname': 'First Name', 
+            'lname': 'Last Name', 
+            'address': 'Address', 
+            'city': 'City', 
+            'province': 'Province', 
+            'postal': 'Postal', 
+            'birthday': 'Birthday', 
+            'gender': 'Gender', 
+            'email': 'Email', 
+            'phone': 'Phone',
+        }
