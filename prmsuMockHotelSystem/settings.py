a413a8578688abd0be2a5da9cc6c10adc65dae60
Dev_tian <c.cleofas@psp.team>
2023-01-21 14:27:15 +0800

    fix the issue and errors in the system

--- prmsuMockHotelSystem/settings.py
+++ prmsuMockHotelSystem/settings.py
@@ -41,8 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'HomePage',
     'LoginForm',
+    'HomePage',
 ]
 
 MIDDLEWARE = [
