3ec562213a42e8a6766aacce2e36172dee3cfffc
Dev_tian <c.cleofas@psp.team>
2023-01-21 14:45:11 +0800

    fix the issue and errors in the system

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'Dashboard/index.html')
+
+def login(request):
+    return render(request, 'Dashboard/login.html')
+
+def register(request):
+    return render(request, 'Dashboard/register.html')
