b9178287c43cb8e8bb16a9d5704e85e3f92dc41f
Dev_tian <c.cleofas@psp.team>
2023-01-21 14:36:54 +0800

    fix the issue and errors in the system

--- prmsuMockHotelSystem/urls.py
+++ prmsuMockHotelSystem/urls.py
@@ -19,7 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('HomePage.urls')),
-
-    # url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
-    # url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}), 
 ]
