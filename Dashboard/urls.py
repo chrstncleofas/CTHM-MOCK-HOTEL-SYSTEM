1e29f8a6668cd5dc43fc1da9cdb2821512f43bff
Dev_tian <c.cleofas@psp.team>
2023-01-21 15:16:05 +0800

    fix the issue and errors in the system

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from Dashboard import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.dashboard, name='dashboard'),
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
 ]
