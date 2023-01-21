329d5821e67c0432dbc0f7d346ce91f0c6bee1aa
Dev_tian <c.cleofas@psp.team>
2023-01-21 08:32:24 +0800

    New Functions

--- HomePage/models.py
+++ HomePage/models.py
@@ -7,11 +7,11 @@ class Reservation(models.Model):
     address = models.CharField(max_length=70)
     city = models.CharField(max_length=70)
     province = models.CharField(max_length=70)
-    postal = models.PositiveIntegerField()
+    postal = models.CharField(max_length=50)
     birthday = models.CharField(max_length=50)
     gender = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
-    phone = models.PositiveIntegerField()
+    phone = models.CharField(max_length=50)
 
     def __str__(self):
         return f'Reservation: {self.fname} {self.lname}'
