82ce3a531bf1dcb27bbbb34a9ef8078b2df1f1cf
Dev_tian <c.cleofas@psp.team>
2023-01-21 10:58:48 +0800

    fix the issue and errors in the system

--- HomePage/views.py
+++ HomePage/views.py
@@ -25,7 +25,36 @@ def addroom(request):
     return render(request, 'HomePage/roomAdd.html')
 
 def bookPage(request):
+    if request.method == 'POST':
+        form = Reservation_Form(request.POST)
+        if form.is_valid():
+
+            new_fname = form.cleaned_data['fname']
+            new_lname = form.cleaned_data['lname']
+            new_address = form.cleaned_data['address']
+            new_city = form.cleaned_data['city']
+            new_province = form.cleaned_data['province']
+            new_postal = form.cleaned_data['postal']
+            new_birthday = form.cleaned_data['birthday']
+            new_gender = form.cleaned_data['gender']
+            new_email = form.cleaned_data['email']
+            new_phone = form.cleaned_data['phone']
+
+            new_reservation = Reservation(
+                fname = new_fname,
+                lname = new_lname,
+                address = new_address,
+                city = new_city,
+                province = new_province,
+                postal = new_postal,
+                birthday = new_birthday,
+                gender = new_gender,
+                email = new_email,
+                phone = new_phone,
+            )
+
     return render(request, 'HomePage/bookPage.html')
+
 
 def contact(request):
     return render(request, 'HomePage/contact.html')
