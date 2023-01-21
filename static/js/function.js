0bb5ced341cadb73c99a06a1cb75a691730500cb
Dev_tian <c.cleofas@psp.team>
2023-01-21 20:14:18 +0800

    fix the issue and errors in the system

---
+++ static/js/function.js
@@ -0,0 +1,9 @@
+$(function() {
+    $(".toggle").on("click", function() {
+        if ($(".item").hasClass("active")) {
+            $(".item").removeClass("active");
+        } else {
+            $(".item").addClass("active");
+        }
+    });
+});
