e93473938d75dc85657a8d6b165e5f64ed5d2c00
Dev_tian <c.cleofas@psp.team>
2023-01-21 11:52:50 +0800

    fix the issue and errors in the system

--- HomePage/models.py
+++ HomePage/models.py
@@ -15,4 +15,8 @@ class Reservation(models.Model):
 
     def __str__(self):
         return f'Reservation: {self.fname} {self.lname}'
-    
+
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    message = models.TextField(max_length=100)
