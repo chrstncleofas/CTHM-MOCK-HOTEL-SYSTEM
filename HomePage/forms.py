c7626040063c2b499be6c4e522a350a676b3b02a
Dev_tian <c.cleofas@psp.team>
2023-01-21 09:52:07 +0800

    fix the issue and errors in the system

--- HomePage/forms.py
+++ HomePage/forms.py
@@ -17,3 +17,15 @@ class Reservation_Form(forms.ModelForm):
             'email': 'Email', 
             'phone': 'Phone',
         }
+        widgets = {
+            'fname': forms.TextInput(attrs={'class': 'form-control'}), 
+            'lname': forms.TextInput(attrs={'class': 'form-control'}), 
+            'address': forms.TextInput(attrs={'class': 'form-control'}), 
+            'city': forms.TextInput(attrs={'class': 'form-control'}), 
+            'province': forms.TextInput(attrs={'class': 'form-control'}), 
+            'postal': forms.TextInput(attrs={'class': 'form-control'}), 
+            'birthday': forms.TextInput(attrs={'class': 'form-control'}), 
+            'gender': forms.TextInput(attrs={'class': 'form-control'}), 
+            'email': forms.TextInput(attrs={'class': 'form-control'}), 
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+        }
