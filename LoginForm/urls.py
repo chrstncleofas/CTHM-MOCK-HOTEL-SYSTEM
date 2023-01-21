f5455edbbaeb61446df8a964aeb81d363c8d4f43
Dev_tian <c.cleofas@psp.team>
2023-01-21 12:45:09 +0800

    fix the issue and errors in the system

--- LoginForm/urls.py
+++ LoginForm/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register),
-    path('login/', views.login, name='login'),
+    path('', views.login, name='login'),
+    path('register/', views.register, name='register'),
 ]
