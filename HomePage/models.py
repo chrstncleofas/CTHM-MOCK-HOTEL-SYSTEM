f9f33bfdb09e3c93a028eb1452a5a6506cb64936
Dev_tian <c.cleofas@psp.team>
2023-01-25 04:55:30 +0800

    Change in UI

--- HomePage/models.py
+++ HomePage/models.py
@@ -8,10 +8,14 @@ class Reservation(models.Model):
     city = models.CharField(max_length=70)
     province = models.CharField(max_length=70)
     postal = models.CharField(max_length=50)
-    birthday = models.CharField(max_length=50)
-    gender = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     phone = models.CharField(max_length=50)
+    arrival_date = models.CharField(max_length=50)
+    arrival_time = models.CharField(max_length=50)
+    departure_date = models.CharField(max_length=50)
+    departure_time = models.CharField(max_length=50)
+    number_person = models.CharField(max_length=50)
+    message = models.CharField(max_length=80)
 
     def __str__(self):
         return f'Reservation: {self.fname} {self.lname}'
